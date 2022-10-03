a91e1a36fe4cde8d0bd12e65ffa76f2aea734222
PeterSalnikov <psalniko@sfu.ca>
2022-10-03 00:50:57 -0700

    kinks ironed out

--- src/Plotter.java
+++ src/Plotter.java
@@ -12,7 +12,7 @@ public class Plotter extends Component {
 
     static double rate = 0.01; //for 10Hz sampling rate, width / 10 and so on
     static double pi = Math.PI;
-    static int scale = 100; // scale for amplitude of plot
+    static int scale = 60; // scale for amplitude of plot; default is 100
 
     int period = 1;
 
@@ -82,7 +82,7 @@ public class Plotter extends Component {
         int bitrate = 4; // with 2 bits, 4 values can be represented
 
         int step = 2*scale / bitrate; //gives the step depending on the scale of the graph
-
+//        System.out.println(step);
         int sign; //to save sign of value after absolution
         double quant;
 
@@ -99,13 +99,18 @@ public class Plotter extends Component {
 
             y = abs(y);
 
-            quant = y + step - y % step;
+            if( y % step == 0 ) //need this as quant would not work if it's already a multiple of the scale
+                quant = y;
+            else {
+                quant = y + step - y % step;
+            }
+//            System.out.println(quant);
 
             int val = (int)quant * sign;
 
             g.drawLine(width + X, height, width + X, height - val);
             g.drawRect(width+X-3,height-val-3,6,6);
-//            System.out.println(quant);
+//            System.out.println(val);
         }
     }
 
@@ -127,8 +132,7 @@ public class Plotter extends Component {
         g.drawLine(padding,height,width * 2 + padding,height); // x-axis
         g.drawLine(padding,0,padding,height * 2); // y-axis
 
-//create a separate loop here for labeling. make an array of chars that you need (0, pi, etc)
-//and bump the x every time you need it. repeat for y, in a different loop. Use DrawString
+//create a separate loop here for labeling. make an array of strings that you need (0, pi, etc)
 //\u03c0 is unicode for pi. I hardcoded this as the assignment only requires up to 2*pi, though the y axis
 //is scalable.
         String[] names = {"0", "\u03c0/2", "\u03c0", "3\u03c0/2", "2\u03c0"};
@@ -141,10 +145,8 @@ public class Plotter extends Component {
 
             ind++;
         }
-
             plotSine(g,period);
             plotQuant(g,period);
-
     }
 
     public static void main(String[] args)
@@ -168,11 +170,7 @@ public class Plotter extends Component {
         {
             function.period = repeat;
             frame.getContentPane().add(function);
-
         }
-
         frame.setVisible(true);
-
     }
-
 }
